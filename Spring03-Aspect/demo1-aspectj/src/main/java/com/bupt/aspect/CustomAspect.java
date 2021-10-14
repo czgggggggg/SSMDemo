f9ef7bc9f7d7a9efc1dda1bb3a3ca34fe53e49b6
czg <1210245540@qq.com>
2021-10-14 21:49:48 +0800

    2021/10/14_2

---
+++ Spring03-Aspect/demo1-aspectj/src/main/java/com/bupt/aspect/CustomAspect.java
@@ -0,0 +1,55 @@
+package com.bupt.aspect;
+
+import org.aspectj.lang.JoinPoint;
+import org.aspectj.lang.ProceedingJoinPoint;
+import org.aspectj.lang.Signature;
+import org.springframework.stereotype.Component;
+
+import java.lang.reflect.Array;
+import java.util.Arrays;
+
+/**
+ * @Author czgggggggg
+ * @Date 2021/10/14
+ * @Description 切面类中的方法：就是做什么事情
+ *              时间：由我们配置
+ */
+@Component
+public class CustomAspect {
+//    public void mybefore(){
+//        System.out.println("before...");
+//    }
+    public void mybefore(JoinPoint joinPoint){
+        System.out.println("before");
+
+        Signature signature = joinPoint.getSignature();
+        String name = signature.getName();
+        System.out.println("method = " + name);
+
+        Object aThis = joinPoint.getThis();
+        Object target = joinPoint.getTarget();
+        Object[] args = joinPoint.getArgs();
+        System.out.println("joinPoint.getThis() = " + aThis);
+        System.out.println("joinPoint.getTarget() = " + target);
+        System.out.println("joinPoint.getArgs() = " + Arrays.asList(args));
+    }
+
+    public void myafter(){
+        System.out.println("after...");
+    }
+
+    public Object myaround(ProceedingJoinPoint proceedingJoinPoint) throws Throwable {
+        System.out.println("around before...");
+        Object proceed = proceedingJoinPoint.proceed();
+        System.out.println("around after...");
+        return proceed;
+    }
+
+    public void myafterReturning(Object object){
+        System.out.println("after-returning object = " + object);
+    }
+
+    public void myafterThrowing(Throwable throwable){
+        System.out.println("after-throwing throwable = " + throwable);
+    }
+}
