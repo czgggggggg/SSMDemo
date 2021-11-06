7728ef6c69d59fbf60de7352ae2217ef6bcc3e39
czg <1210245540@qq.com>
2021-11-06 16:51:31 +0800

    2021/11/06_2

--- Spring04-Tx/demo3-tx_template/src/main/java/com/bupt/service/AccountServiceImpl.java
+++ Spring04-Tx/demo3-tx_template/src/main/java/com/bupt/service/AccountServiceImpl.java
@@ -3,6 +3,9 @@ package com.bupt.service;
 import com.bupt.dao.AccountDao;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.TransactionStatus;
+import org.springframework.transaction.support.TransactionCallbackWithoutResult;
+import org.springframework.transaction.support.TransactionTemplate;
 
 /**
  * @Author czgggggggg
@@ -11,15 +14,37 @@ import org.springframework.stereotype.Service;
  */
 @Service
 public class AccountServiceImpl implements AccountService {
+    //没有采用事务的代码
+//    @Autowired
+//    AccountDao accountDao;
+//
+//    public void transfer(int fromId, int toId, int money) {
+//        int fromMoney = accountDao.selectMoneyById(fromId) - money;//转账者少了钱
+//        int toMoney = accountDao.selectMoneyById(toId) + money;    //收账者多了钱
+//
+//        accountDao.updateMoneyById(fromId,fromMoney);
+//        int i = 1 / 0;
+//        accountDao.updateMoneyById(toId,toMoney);
+//    }
+
+    //采用了事务的代码
     @Autowired
     AccountDao accountDao;
+    @Autowired
+    TransactionTemplate transactionTemplate;
 
-    public void transfer(int fromId, int toId, int money) {
-        int fromMoney = accountDao.selectMoneyById(fromId) - money;//转账者少了钱
-        int toMoney = accountDao.selectMoneyById(toId) + money;    //收账者多了钱
+    public void transfer(final int fromId, final int toId, int money) {
+        final int fromMoney = accountDao.selectMoneyById(fromId) - money;//转账者少了钱
+        final int toMoney = accountDao.selectMoneyById(toId) + money;    //收账者多了钱
 
-        accountDao.updateMoneyById(fromId,fromMoney);
-        int i = 1 / 0;
-        accountDao.updateMoneyById(toId,toMoney);
+        //侵入式的编程 → 改变了原有的编码
+        transactionTemplate.execute(new TransactionCallbackWithoutResult() {
+            @Override
+            protected void doInTransactionWithoutResult(TransactionStatus transactionStatus) {
+                accountDao.updateMoneyById(fromId,fromMoney);
+                int i = 1 / 0;
+                accountDao.updateMoneyById(toId,toMoney);
+            }
+        });
     }
 }
