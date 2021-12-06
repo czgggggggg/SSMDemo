f01a08e0658939e5256681f46decb52f673dbdde
czg <1210245540@qq.com>
2021-12-06 19:25:54 +0800

    20211206_03

---
+++ Spring04-Tx/demo6-tx_annotation/src/main/java/com/bupt/service/AccountServiceImpl.java
@@ -0,0 +1,48 @@
+package com.bupt.service;
+
+import com.bupt.dao.AccountDao;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Isolation;
+import org.springframework.transaction.annotation.Propagation;
+import org.springframework.transaction.annotation.Transactional;
+
+/**
+ * @Author czgggggggg
+ * @Date 2021/11/6
+ * @Description
+ */
+@Service
+public class AccountServiceImpl implements AccountService {
+//    //没有采用事务的代码
+//    @Autowired
+//    AccountDao accountDao;
+//
+//    public void transfer(int fromId, int toId, int money) {
+//        int fromMoney = accountDao.selectMoneyById(fromId) - money;//转账者少了钱
+//        int toMoney = accountDao.selectMoneyById(toId) + money;    //收账者多了钱
+//
+//        accountDao.updateMoneyById(fromId,fromMoney);
+//        int i = 1 / 0;
+//        accountDao.updateMoneyById(toId,toMoney);
+//    }
+
+
+    @Autowired
+    AccountDao accountDao;
+
+    @Transactional(isolation = Isolation.REPEATABLE_READ,propagation = Propagation.REQUIRED,
+                   readOnly = true,noRollbackFor = java.lang.ArithmeticException.class)
+//    @Transactional
+    public void transfer(int fromId, int toId, int money) {
+        int fromMoney = accountDao.selectMoneyById(fromId) - money;//转账者少了钱
+        int toMoney = accountDao.selectMoneyById(toId) + money;    //收账者多了钱
+
+        accountDao.updateMoneyById(fromId,fromMoney);
+        int i = 1 / 0;
+        accountDao.updateMoneyById(toId,toMoney);
+    }
+
+
+
+}
